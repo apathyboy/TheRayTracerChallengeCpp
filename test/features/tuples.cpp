0a9275427369decca5549d6badd3072960d35b04
Eric Barr <eb@eb2.co>
2019-04-23 04:02:35 -0500

    Added test for vector

--- test/features/tuples.cpp
+++ test/features/tuples.cpp
@@ -19,3 +19,18 @@ SCENARIO("A tuple with w=1.0 is a point", "[tuples]")
         AND_THEN("a is not a vector") { REQUIRE_FALSE(rtc::is_vector(a)); }
     }
 }
+
+SCENARIO("A tuple with w=0 is a vector", "[tuples]")
+{
+    GIVEN("a = tuple(4.3, -4.2, 3.1, 0.0)")
+    {
+        auto a = rtc::tuple_t{4.3f, -4.2f, 3.1f, 0.f};
+
+        THEN("a.x == 4.3") { REQUIRE(a.x == 4.3_a); }
+        AND_THEN("a.y == -4.2") { REQUIRE(a.y == -4.2_a); }
+        AND_THEN("a.z == 3.1") { REQUIRE(a.z == 3.1_a); }
+        AND_THEN("a.w == 0.0") { REQUIRE(a.w == 0.0_a); }
+        AND_THEN("a is not a point") { REQUIRE_FALSE(rtc::is_point(a)); }
+        AND_THEN("a is a vector") { REQUIRE(rtc::is_vector(a)); }
+    }
+}

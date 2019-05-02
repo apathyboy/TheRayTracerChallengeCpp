04f31585df8cdac983ec8ce8024e29fbd68f02be
ap <eb@eb2.co>
2019-05-02 07:01:55 -0500

    Chapter 2 implementation

---
+++ apps/chapter2.cpp
@@ -0,0 +1,51 @@
+
+#include <rtc/canvas.hpp>
+#include <rtc/tuple.hpp>
+
+struct projectile_t {
+    rtc::tuple_t position;
+    rtc::tuple_t velocity;
+};
+
+struct environment_t {
+    rtc::tuple_t gravity;
+    rtc::tuple_t wind;
+};
+
+static projectile_t tick(const environment_t& env, const projectile_t& proj)
+{
+    auto position = proj.position + proj.velocity;
+    auto velocity = proj.velocity + env.gravity + env.wind;
+
+    return {position, velocity};
+}
+
+int main()
+{
+    auto start    = rtc::point(0, 1, 0);
+    auto velocity = rtc::normalize(rtc::vector(1, 1.8f, 0)) * 11.25f;
+    auto proj     = projectile_t{start, velocity};
+
+    auto gravity = rtc::vector(0, -0.1f, 0);
+    auto wind    = rtc::vector(-0.01f, 0, 0);
+    auto env     = environment_t{gravity, wind};
+
+    auto canvas = rtc::canvas_t{900, 550};
+    auto color  = rtc::color(1, 1, 1);
+
+    while (proj.position.y > 0.f) {
+        proj = tick(env, proj);
+
+        auto x = static_cast<uint32_t>(std::roundf(proj.position.x));
+        auto y = static_cast<uint32_t>(
+            std::roundf(canvas.height() - proj.position.y));
+
+        if (x < canvas.width() && y < canvas.height()) {
+            canvas.write_pixel(x, y, color);
+        }
+    }
+
+    rtc::write_bmp_to_file("chapter2.bmp", rtc::canvas_to_bmp(canvas));
+
+    return 0;
+}

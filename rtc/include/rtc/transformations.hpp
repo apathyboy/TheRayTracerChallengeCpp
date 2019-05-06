430b9235e31f99177732d8078b889a660cb7320c
ap <eb@eb2.co>
2019-05-06 07:48:16 -0500

    scaling

--- rtc/include/rtc/transformations.hpp
+++ rtc/include/rtc/transformations.hpp
@@ -6,12 +6,22 @@
 
 namespace rtc {
 
-inline matrix4x4_t translation(float x, float y, float z)
+[[nodiscard]] inline matrix4x4_t translation(float x, float y, float z) noexcept
 {
     // clang-format off
     return {1, 0, 0, x,
             0, 1, 0, y,
             0, 0, 1, z,
+            0, 0, 0, 1};
+    // clang-format on
+}
+
+[[nodiscard]] inline matrix4x4_t scaling(float x, float y, float z) noexcept
+{
+    // clang-format off
+    return {x, 0, 0, 0,
+            0, y, 0, 0,
+            0, 0, z, 0,
             0, 0, 0, 1};
     // clang-format on
 }
--- test/features/transformations.cpp
+++ test/features/transformations.cpp
@@ -3,7 +3,7 @@
 
 #include <catch2/catch.hpp>
 
-SCENARIO("Multiplying by a translation matrix", "[matrices]")
+SCENARIO("Multiplying by a translation matrix", "[transformations]")
 {
     GIVEN("transform = translation(5, -3, 2)")
     {
@@ -21,7 +21,7 @@ SCENARIO("Multiplying by a translation matrix", "[matrices]")
     }
 }
 
-SCENARIO("Multiplying by the inverse of a translation matrix", "[matrices]")
+SCENARIO("Multiplying by the inverse of a translation matrix", "[transformations]")
 {
     GIVEN("transform = translation(5, -3, 2)")
     {
@@ -44,7 +44,7 @@ SCENARIO("Multiplying by the inverse of a translation matrix", "[matrices]")
     }
 }
 
-SCENARIO("Translation does not affect vectors", "[matrices]")
+SCENARIO("Translation does not affect vectors", "[transformations]")
 {
     GIVEN("transform = translation(5, -3, 2)")
     {
@@ -55,6 +55,83 @@ SCENARIO("Translation does not affect vectors", "[matrices]")
             auto v = rtc::vector(-3, 4, 5);
 
             THEN("transform * v == v") { REQUIRE(transform * v == v); }
+        }
+    }
+}
+
+SCENARIO("A scaling matrix applied to a point", "[transformations]")
+{
+    GIVEN("transform = scaling(2, 3, 4)")
+    {
+        auto transform = rtc::scaling(2, 3, 4);
+
+        AND_GIVEN("p = point(-4, 6, 8)")
+        {
+            auto p = rtc::point(-4, 6, 8);
+
+            THEN("transform * p == point(-8, 18, 32)")
+            {
+                REQUIRE(transform * p == rtc::point(-8, 18, 32));
+            }
+        }
+    }
+}
+
+SCENARIO("A scaling matrix applied to a vector", "[transformations]")
+{
+    GIVEN("transform = scaling(2, 3, 4)")
+    {
+        auto transform = rtc::scaling(2, 3, 4);
+
+        AND_GIVEN("v = vector(-4, 6, 8)")
+        {
+            auto v = rtc::vector(-4, 6, 8);
+
+            THEN("transform * v == vector(-8, 18, 32)")
+            {
+                REQUIRE(transform * v == rtc::vector(-8, 18, 32));
+            }
+        }
+    }
+}
+
+SCENARIO("Multiplying by the inverse of a scaling matrix", "[transformations]")
+{
+    GIVEN("transform = scaling(2, 3, 4)")
+    {
+        auto transform = rtc::scaling(2, 3, 4);
+
+        AND_GIVEN("inv = inverse(transform)")
+        {
+            auto inv = rtc::inverse(transform);
+
+            AND_GIVEN("v = vector(-4, 6, 8)")
+            {
+                auto v = rtc::vector(-4, 6, 8);
+
+                THEN("inv * v == vector(-2, 2, 2)")
+                {
+                    REQUIRE(inv * v == rtc::vector(-2, 2, 2));
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Reflection is scaling by a negative value", "[transformations]")
+{
+    GIVEN("transform = scaling(-1, 1, 1)")
+    {
+        auto transform = rtc::scaling(-1, 1, 1);
+
+        AND_GIVEN("p = point(2, 3, 4)")
+        {
+            auto p = rtc::point(2, 3, 4);
+
+            THEN("transform * p = point(-2, 3, 4)")
+            {
+                REQUIRE(transform * p == rtc::point(-2, 3, 4));
+            }
         }
     }
 }

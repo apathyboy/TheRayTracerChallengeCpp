5ba04b8d27f84073dca7a12bb72f880c83f08c88
ap <eb@eb2.co>
2019-05-08 06:37:15 -0500

    lighting calculation

--- rtc/include/rtc/materials.hpp
+++ rtc/include/rtc/materials.hpp
@@ -2,6 +2,7 @@
 #ifndef RTC_MATERIALS_HPP_
 #define RTC_MATERIALS_HPP_
 
+#include "lights.hpp"
 #include "math.hpp"
 #include "tuple.hpp"
 
@@ -27,6 +28,35 @@ struct material_t {
 [[nodiscard]] inline material_t material() noexcept
 {
     return {rtc::color(1, 1, 1), 0.1f, 0.9f, 0.9f, 200.f};
+}
+
+[[nodiscard]] inline tuple4_t lighting(const material_t&    material,
+                                       const point_light_t& light,
+                                       const tuple4_t&      position,
+                                       const tuple4_t&      eyev,
+                                       const tuple4_t&      normalv)
+{
+    auto effective_color  = material.color * light.intensity;
+    auto lightv           = rtc::normalize(light.position - position);
+    auto ambient          = effective_color * material.ambient;
+    auto light_dot_normal = rtc::dot(lightv, normalv);
+
+    tuple4_t diffuse  = {};
+    tuple4_t specular = {};
+
+    if (light_dot_normal >= 0) {
+        diffuse = effective_color * material.diffuse * light_dot_normal;
+
+        auto reflectv        = rtc::reflect(-lightv, normalv);
+        auto reflect_dot_eye = rtc::dot(reflectv, eyev);
+
+        if (reflect_dot_eye > 0) {
+            auto factor = std::powf(reflect_dot_eye, material.shininess);
+            specular    = light.intensity * material.specular * factor;
+        }
+    }
+
+    return ambient + diffuse + specular;
 }
 
 } // namespace rtc
--- test/features/materials.cpp
+++ test/features/materials.cpp
@@ -1,4 +1,5 @@
 
+#include <rtc/lights.hpp>
 #include <rtc/materials.hpp>
 #include <rtc/tuple.hpp>
 
@@ -15,5 +16,214 @@ SCENARIO("The default material", "[materials]")
         AND_THEN("m.diffuse == 0.9") {}
         AND_THEN("m.specular == 0.9") {}
         AND_THEN("m.shininess == 200.0") {}
+    }
+}
+
+SCENARIO("Lighting with eye between light and surface", "[materials]")
+{
+    GIVEN("m = material()")
+    {
+        auto m = rtc::material();
+
+        AND_GIVEN("position = point(0, 0, 0)")
+        {
+            auto position = rtc::point(0, 0, 0);
+
+            AND_GIVEN("eyev = vector(0, 0, -1)")
+            {
+                auto eyev = rtc::vector(0, 0, -1);
+
+                AND_GIVEN("normalv = vector(0, 0, -1)")
+                {
+                    auto normalv = rtc::vector(0, 0, -1);
+
+                    AND_GIVEN(
+                        "light = point_light(point(0, 0, -10), color(1, 1, 1))")
+                    {
+                        auto light = rtc::point_light_t{rtc::point(0, 0, -10),
+                                                        rtc::color(1, 1, 1)};
+
+                        WHEN("result = lighting(m, light, position, eyev, normalv)")
+                        {
+                            auto result = rtc::lighting(
+                                m, light, position, eyev, normalv);
+
+                            THEN("result == color(1.9, 1.9, 1.9)")
+                            {
+                                REQUIRE(result == rtc::color(1.9f, 1.9f, 1.9f));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Lighting with eye between light and surface, eye offset 45 degrees",
+         "[materials]")
+{
+    GIVEN("m = material()")
+    {
+        auto m = rtc::material();
+
+        AND_GIVEN("position = point(0, 0, 0)")
+        {
+            auto position = rtc::point(0, 0, 0);
+
+            AND_GIVEN("eyev = vector(0, sqrt(2)/2, -sqrt(2)/2)")
+            {
+                auto eyev = rtc::vector(0, std::sqrtf(2) / 2, -std::sqrtf(2) / 2);
+
+                AND_GIVEN("normalv = vector(0, 0, -1)")
+                {
+                    auto normalv = rtc::vector(0, 0, -1);
+
+                    AND_GIVEN(
+                        "light = point_light(point(0, 0, -10), color(1, 1, 1))")
+                    {
+                        auto light = rtc::point_light_t{rtc::point(0, 0, -10),
+                                                        rtc::color(1, 1, 1)};
+
+                        WHEN("result = lighting(m, light, position, eyev, normalv)")
+                        {
+                            auto result = rtc::lighting(
+                                m, light, position, eyev, normalv);
+
+                            THEN("result = color(1.0, 1.0, 1.0)")
+                            {
+                                REQUIRE(result == rtc::color(1.f, 1.f, 1.f));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Lighting with eye opposite surface, light offest 45 degrees",
+         "[materials]")
+{
+    GIVEN("m = material()")
+    {
+        auto m = rtc::material();
+
+        AND_GIVEN("position = point(0, 0, 0)")
+        {
+            auto position = rtc::point(0, 0, 0);
+
+            AND_GIVEN("eyev = vector(0, 0, -1)")
+            {
+                auto eyev = rtc::vector(0, 0, -1);
+
+                AND_GIVEN("normalv = vector(0, 0, -1)")
+                {
+                    auto normalv = rtc::vector(0, 0, -1);
+
+                    AND_GIVEN(
+                        "light = point_light(point(0, 10, -10), color(1, 1, 1))")
+                    {
+                        auto light = rtc::point_light_t{rtc::point(0, 10, -10),
+                                                        rtc::color(1, 1, 1)};
+
+                        WHEN("result = lighting(m, light, position, eyev, normalv)")
+                        {
+                            auto result = rtc::lighting(
+                                m, light, position, eyev, normalv);
+
+                            THEN("result == color(0.7364, 0.7364, 0.7364)")
+                            {
+                                REQUIRE(result
+                                        == rtc::color(0.7364f, 0.7364f, 0.7364f));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Lighting with eye in the path of the reflection vector", "[materials]")
+{
+    GIVEN("m = material()")
+    {
+        auto m = rtc::material();
+
+        AND_GIVEN("position = point(0, 0, 0)")
+        {
+            auto position = rtc::point(0, 0, 0);
+
+            AND_GIVEN("eyev = vector(0, -sqrt(2)/2, -sqrt(2)/2)")
+            {
+                auto eyev = rtc::vector(0, -std::sqrtf(2) / 2, -std::sqrtf(2) / 2);
+
+                AND_GIVEN("normalv = vector(0, 0, -1)")
+                {
+                    auto normalv = rtc::vector(0, 0, -1);
+
+                    AND_GIVEN(
+                        "light = point_light(point(0, 10, -10), color(1, 1, 1))")
+                    {
+                        auto light = rtc::point_light_t{rtc::point(0, 10, -10),
+                                                        rtc::color(1, 1, 1)};
+
+                        WHEN("result = lighting(m, light, position, eyev, normalv)")
+                        {
+                            auto result = rtc::lighting(
+                                m, light, position, eyev, normalv);
+
+                            THEN("result = color(1.6364, 1.6364, 1.6364)")
+                            {
+                                REQUIRE(result
+                                        == rtc::color(1.6364f, 1.6364f, 1.6364f));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+SCENARIO("Lighting with the light behind the surface", "[materials]")
+{
+    GIVEN("m = material()")
+    {
+        auto m = rtc::material();
+
+        AND_GIVEN("position = point(0, 0, 0)")
+        {
+            auto position = rtc::point(0, 0, 0);
+
+            AND_GIVEN("eyev = vector(0, 0, -1)")
+            {
+                auto eyev = rtc::vector(0, 0, -1);
+
+                AND_GIVEN("normalv = vector(0, 0, -1)")
+                {
+                    auto normalv = rtc::vector(0, 0, -1);
+
+                    AND_GIVEN(
+                        "light = point_light(point(0, 0, 10), color(1, 1, 1))")
+                    {
+                        auto light = rtc::point_light_t{rtc::point(0, 0, 10),
+                                                        rtc::color(1, 1, 1)};
+
+                        WHEN("result = lighting(m, light, position, eyev, normalv)")
+                        {
+                            auto result = rtc::lighting(
+                                m, light, position, eyev, normalv);
+
+                            THEN("result == color(0.1, 0.1, 0.1)")
+                            {
+                                REQUIRE(result == rtc::color(0.1f, 0.1f, 0.1f));
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
